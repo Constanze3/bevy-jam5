7410481fb04b836368bf426706cacf1b6a5f0f5a
Constanze3 <81444666+Constanze3@users.noreply.github.com>
2024-07-23 23:49:39 +0200

    new town model, inspector plugin

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,3 @@
-//! A basic implementation of a character controller for a kinematic rigid body.
-//!
-//! This showcases the following:
-//!
-//! - Basic directional movement and jumping
-//! - Support for both keyboard and gamepad input
-//! - A configurable maximum slope angle
-//! - Collision response for kinematic bodies
-//! - Loading a platformer environment from a glTF
-//!
-//! The character controller logic is contained within the `plugin` module.
-//!
-//! For a dynamic character controller, see the `dynamic_character_3d` example.
-//!
-//! ## Warning
-//!
-//! Note that this is *not* intended to be a fully featured character controller,
-//! and the collision logic is quite basic.
-//!
-//! For a better solution, consider implementing a "collide-and-slide" algorithm,
-//! or use an existing third party character controller plugin like Bevy Tnua
-//! (a dynamic character controller).
-
 mod cameras;
 mod car_controller;
 mod cubemap_factory;
@@ -33,10 +10,8 @@ use avian3d::{math::*, prelude::*};
 use bevy::{core_pipeline::Skybox, prelude::*};
 use bevy_flycam::prelude::PlayerPlugin;
 
-use cameras::*;
-// use car_controller::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use cubemap_factory::*;
-use plugin::*;
 use resources::*;
 use simulation_state::*;
 
@@ -47,26 +22,28 @@ fn main() {
             DefaultPlugins,
             PhysicsPlugins::default(),
             SimulationStatePlugin,
-            // CharacterControllerPlugin,
-            // CarControllerPlugin,
+            WorldInspectorPlugin::new(),
             PlayerPlugin,
             CubemapFactoryPlugin,
         ))
         .add_systems(Startup, (setup_world).chain())
+        .init_state::<TestSkyboxState>()
         .add_systems(
             Update,
             test_skybox.run_if(in_state(TestSkyboxState::Waiting)),
         )
-        .insert_state(TestSkyboxState::Waiting)
         .run();
 }
 
-#[derive(States, Hash, PartialEq, Eq, Debug, Clone)]
+#[derive(States, Clone, PartialEq, Eq, Hash, Debug, Default)]
 enum TestSkyboxState {
+    #[default]
     Waiting,
     Done,
 }
 
+/// This system adds a skybox to the camera after it is loaded.
+/// `TODO` properly initialize skybox instead.
 fn test_skybox(
     cameras: Query<Entity, With<Camera>>,
     mut commands: Commands,
@@ -90,7 +67,6 @@ fn test_skybox(
 }
 
 fn setup_world(mut commands: Commands, assets: Res<AssetServer>) {
-    // Environment (see the `collider_constructors` example for creating colliders from scenes)
     commands.spawn((
         SceneBundle {
             scene: assets.load("town.glb#Scene0"),
@@ -103,7 +79,7 @@ fn setup_world(mut commands: Commands, assets: Res<AssetServer>) {
 
     commands.insert_resource(AmbientLight {
         color: Color::srgb_u8(182, 205, 214),
-        brightness: 200.0,
+        brightness: 500.0,
     });
 
     commands.spawn(DirectionalLightBundle {
@@ -114,72 +90,9 @@ fn setup_world(mut commands: Commands, assets: Res<AssetServer>) {
         },
         transform: Transform {
             translation: Vec3::new(0.0, 2.0, 0.0),
-            rotation: Quat::from_rotation_x(-PI / 1.8),
+            rotation: Quat::from_euler(EulerRot::XYZ, 4.0, -0.7, 0.0),
             ..default()
         },
         ..default()
     });
-}
-
-fn setup_player(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    assets: Res<AssetServer>,
-) {
-    // Player
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Capsule3d::new(0.4, 1.0)),
-            material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
-            transform: Transform::from_xyz(0.0, 1.5, 0.0),
-            ..default()
-        },
-        CharacterControllerBundle::new(Collider::capsule(0.4, 1.0), Vector::NEG_Y * 9.81 * 2.0)
-            .with_movement(30.0, 0.92, 7.0, (30.0 as Scalar).to_radians()),
-    ));
-
-    // A cube to move around
-    commands.spawn((
-        RigidBody::Dynamic,
-        Collider::cuboid(1.0, 1.0, 1.0),
-        PbrBundle {
-            mesh: meshes.add(Cuboid::default()),
-            material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
-            transform: Transform::from_xyz(3.0, 2.0, 3.0),
-            ..default()
-        },
-    ));
-
-    // Environment (see the `collider_constructors` example for creating colliders from scenes)
-    commands.spawn((
-        SceneBundle {
-            scene: assets.load("town.glb#Scene0"),
-            transform: Transform::default(),
-            ..default()
-        },
-        ColliderConstructorHierarchy::new(ColliderConstructor::ConvexHullFromMesh),
-        RigidBody::Static,
-    ));
-
-    // Light
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 2_000_000.0,
-            range: 50.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(0.0, 15.0, 0.0),
-        ..default()
-    });
-
-    // Camera
-    commands.spawn((
-        MainCamera,
-        Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 2.0, 0.0),
-            ..default()
-        },
-    ));
 }
