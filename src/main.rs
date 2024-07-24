f06a188c120a947c86b0359d6c00b0e537afbf9a
Constanze3 <81444666+Constanze3@users.noreply.github.com>
2024-07-24 12:15:08 +0200

    outline post processing effects, town with lamps

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 mod cameras;
 mod car_controller;
 mod cubemap_factory;
+mod player_controller;
 mod resources;
 mod simulation_state;
 mod utils;
-mod player_controller;
 
 use avian3d::{math::*, prelude::*};
-use bevy::{core_pipeline::Skybox, prelude::*};
-use bevy::{input::mouse::MouseMotion, prelude::*};
-use bevy_camera_extras::{components::{AttachedTo, CameraControls}, plugins::CameraExtrasPlugin};
+use bevy::{
+    core_pipeline::{prepass::NormalPrepass, Skybox},
+    prelude::*,
+};
+use bevy_camera_extras::{
+    components::{AttachedTo, CameraControls},
+    plugins::CameraExtrasPlugin,
+};
 
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_outline_post_process::{components::OutlinePostProcessSettings, OutlinePostProcessPlugin};
 use player_controller::plugins::*;
 
 use cubemap_factory::*;
@@ -30,7 +36,9 @@ fn main() {
             //PlayerPlugin,
             CubemapFactoryPlugin,
             CharacterControllerPlugin,
+            OutlinePostProcessPlugin,
         ))
+        .insert_resource(Msaa::Off)
         .add_systems(Startup, (setup_world).chain())
         .init_state::<TestSkyboxState>()
         .add_systems(
@@ -44,7 +52,6 @@ fn main() {
         })
         //.add_systems(Update, close_on_esc)
         .insert_resource(MovementSettings::default())
-
         .run();
 }
 
@@ -69,42 +76,49 @@ fn test_skybox(
         return;
     }
     println!("camera count: {:#?}", cameras.iter().len());
-    commands
-        .entity(cameras.get_single().unwrap())
-        .insert(Skybox {
+    commands.entity(cameras.get_single().unwrap()).insert((
+        Skybox {
             image: cubemap_factory.load_from_folder("sky", assets, images),
             brightness: 1000.0,
-        });
+        },
+        NormalPrepass,
+        OutlinePostProcessSettings::new(1.5, 0.0, false),
+    ));
 
     next_state.set(TestSkyboxState::Done);
 }
 
-fn setup_world(mut commands: Commands, assets: Res<AssetServer>, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
-    // Player
-    let player = commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Capsule3d::new(0.4, 1.0)),
-            material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
-            transform: Transform::from_xyz(0.0, 1.5, 0.0),
-            ..default()
-        },
-        CharacterControllerBundle::new(Collider::capsule(0.4, 1.0), Vector::NEG_Y * 9.81 * 2.0)
-            .with_movement(30.0, 0.92, 7.0, (30.0 as Scalar).to_radians()),
-    )).id();
-
-    // camera
-    commands.spawn(
-        (
-            Camera3dBundle {
-                transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+fn setup_world(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // player
+    let player = commands
+        .spawn((
+            PbrBundle {
+                mesh: meshes.add(Capsule3d::new(0.4, 1.0)),
+                material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
+                transform: Transform::from_xyz(0.0, 1.5, 0.0),
                 ..default()
             },
-            CameraControls,
-            AttachedTo(player)
-        )
-    );
+            CharacterControllerBundle::new(Collider::capsule(0.4, 1.0), Vector::NEG_Y * 9.81 * 2.0)
+                .with_movement(30.0, 0.92, 7.0, (30.0 as Scalar).to_radians()),
+        ))
+        .id();
 
-    // A cube to move around
+    // camera
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        CameraControls,
+        AttachedTo(player),
+    ));
+
+    // a cube to move around
     commands.spawn((
         RigidBody::Dynamic,
         Collider::cuboid(1.0, 1.0, 1.0),
@@ -116,24 +130,24 @@ fn setup_world(mut commands: Commands, assets: Res<AssetServer>, mut meshes: Res
         },
     ));
 
-    // Environment (see the `collider_constructors` example for creating colliders from scenes)
+    // town
     commands.spawn((
         SceneBundle {
             scene: assets.load("town.glb#Scene0"),
             transform: Transform::default(),
             ..default()
         },
-        ColliderConstructorHierarchy::new(ColliderConstructor::ConvexHullFromMesh),
+        ColliderConstructorHierarchy::new(ColliderConstructor::TrimeshFromMesh),
         RigidBody::Static,
     ));
 
+    // ambient light
     commands.insert_resource(AmbientLight {
         color: Color::srgb_u8(182, 205, 214),
         brightness: 500.0,
     });
 
-    commands.insert_resource(AmbientLight::default());
-
+    // sunlight
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             illuminance: light_consts::lux::OVERCAST_DAY,
@@ -148,38 +162,3 @@ fn setup_world(mut commands: Commands, assets: Res<AssetServer>, mut meshes: Res
         ..default()
     });
 }
-
-// #[derive(Component)]
-// struct MainCamera;
-
-// #[derive(Resource)]
-// struct MovementSettings {
-//     sensitivity: f32,
-// }
-
-// impl Default for MovementSettings {
-//     fn default() -> Self {
-//         return Self { sensitivity: 0.1 };
-//     }
-// }
-
-// fn camera_control(
-//     //mut q_camera: Query<&mut Transform, With<MainCamera>>,
-//     mut evr_mouse_motion: EventReader<MouseMotion>,
-//     movement_settings: Res<MovementSettings>,
-// ) {
-//     let sensitivity = movement_settings.sensitivity;
-
-//     let mut transform = q_camera.get_single_mut().unwrap();
-//     for ev in evr_mouse_motion.read() {
-//         let (mut yaw, mut pitch, _) = transform.rotation.to_euler(EulerRot::YXZ);
-
-//         yaw -= (ev.delta.x * sensitivity).to_radians();
-//         pitch -= (ev.delta.y * sensitivity).to_radians();
-
-//         pitch = pitch.clamp(-1.54, 1.54);
-
-//         transform.rotation =
-//             Quat::from_axis_angle(Vec3::Y, yaw) * Quat::from_axis_angle(Vec3::X, pitch);
-//     }
-// }
