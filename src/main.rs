bb8d3d38fac7b42a751a688c5380538bcce181c0
Constanze3 <81444666+Constanze3@users.noreply.github.com>
2024-07-29 11:27:20 +0200

    ci template

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+// Bevy code commonly triggers these lints and they may be important signals
+// about code quality. They are sometimes hard to avoid though, and the CI
+// workflow treats them as errors, so this allows them throughout the project.
+// Feel free to delete this line.
+#![allow(clippy::too_many_arguments, clippy::type_complexity)]
+
 use asset_loading::AssetLoaderPlugin;
 use avian3d::prelude::*;
 use bevy::{
+    asset::AssetMetaCheck,
     core_pipeline::{prepass::NormalPrepass, Skybox},
     prelude::*,
 };
@@ -26,7 +33,13 @@ fn main() {
             home::plugin,
         ))
         .add_plugins((
-            DefaultPlugins,
+            DefaultPlugins.set(AssetPlugin {
+                // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                // This causes errors and even panics in web builds on itch.
+                // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                meta_check: AssetMetaCheck::Never,
+                ..default()
+            }),
             PhysicsPlugins::default(),
             SimulationStatePlugin,
             WorldInspectorPlugin::new(),
