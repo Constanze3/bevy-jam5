09b9ad2be7c9367da3c2e70c3e98c021f790a298
Constanze3 <81444666+Constanze3@users.noreply.github.com>
2024-07-29 18:31:10 +0200

    music

--- src/asset_loading.rs
+++ src/asset_loading.rs
@@ -6,7 +6,7 @@ pub struct AssetLoaderPlugin;
 
 impl Plugin for AssetLoaderPlugin {
     fn build(&self, app: &mut App) {
-        app.add_loading_state(
+        app.add_systems(Startup, setup).add_loading_state(
             LoadingState::new(GameState::Loading)
                 .continue_to_state(GameState::Spawning)
                 .load_collection::<GltfAssets>(),
@@ -18,4 +18,12 @@ impl Plugin for AssetLoaderPlugin {
 pub struct GltfAssets {
     #[asset(path = "town.glb")]
     pub world: Handle<Gltf>,
+}
+
+fn setup(asset_server: Res<AssetServer>, mut commands: Commands) {
+    commands.spawn(AudioBundle {
+        source: asset_server.load("music.ogg"),
+        settings: PlaybackSettings::LOOP,
+        ..default()
+    });
 }
